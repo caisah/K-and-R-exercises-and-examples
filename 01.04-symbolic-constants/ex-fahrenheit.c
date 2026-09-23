0f8a468257c6ca76206c0044e8c0ada643670409
ferrarimas <149778779+ferrarimas@users.noreply.github.com>
2026-09-23 03:22:43 -0300

    Refactor for loop with Celsius conversion comments
    for didatics purposes, use cels var to make code more readable.

--- 01.04-symbolic-constants/ex-fahrenheit.c
+++ 01.04-symbolic-constants/ex-fahrenheit.c
@@ -9,7 +9,8 @@ int main(void)
 {
   int fahr;
 
-  for(fahr = LOWER; fahr <= UPPER; fahr += STEP)
+  for(fahr = LOWER; fahr <= UPPER; fahr += STEP)  // int cels=(5/9)*(fahr-32); 
+                                                  // printf("%3d %6.1f\n", fahr,cels);
     printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
   return 0;
